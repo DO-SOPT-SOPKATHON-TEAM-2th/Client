6ba85c3cdb583ef55cdfb29d5cbecd46f2c05739
Doyun Lee <doyn511@ajou.ac.kr>
2023-11-26 06:46:15 +0900

    [feat / Main] Main페이지 UI 구현

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
+import { MainWriteIc, NavigationRightIc } from '../../assets';
 import mainImg from '../../assets/images/mainImg@2x.png';
 import Header from '../../components/Main/Header';
 import calculateDate from '../../utils/CalculateDate';
+
 /** Main page */
 const Main = () => {
   const { Month, Day } = calculateDate();
@@ -11,6 +13,7 @@ const Main = () => {
 
   return (
     <St.MainWrapper>
+      <St.RemainContainer></St.RemainContainer>
       <Header />
       <St.CountWrapper>
         내일을 기대하는 사람이
@@ -24,19 +27,19 @@ const Main = () => {
         </St.MainTextWrapper>
         <St.MainImage src={mainImg} alt="메인이미지" />
       </St.MainImageWrapper>
-      <div>
-        <button>
-          {/* 아이콘 */}
-          <div>
-            <span>내일은 무슨 일이 일어나나요?</span>
-            {/* 아이콘 */}
-          </div>
-        </button>
-        <button>
+      <St.ButtonWrapper>
+        <St.WritingBtn>
+          <MainWriteIc />
+          <St.WritingTextWrapper>
+            내일은 무슨 일이 일어나나요?
+            <NavigationRightIc />
+          </St.WritingTextWrapper>
+        </St.WritingBtn>
+        <St.ViewBtn>
           <span>누군가의 내일 보러가기</span>
-          {/* icon */}
-        </button>
-      </div>
+          <NavigationRightIc />
+        </St.ViewBtn>
+      </St.ButtonWrapper>
     </St.MainWrapper>
   );
 };
@@ -55,6 +58,9 @@ const St = {
     background-color: ${({ theme }) => theme.colors.black};
   `,
 
+  RemainContainer: styled.div`
+    height: 4.4rem;
+  `,
   CountWrapper: styled.div`
     display: flex;
     justify-content: center;
@@ -109,5 +115,39 @@ const St = {
     width: 30rem;
     height: 16.4rem;
     margin-top: 1.7rem;
+  `,
+
+  ButtonWrapper: styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 1.5rem;
+    ${({ theme }) => theme.fonts.Body};
+    color: ${({ theme }) => theme.colors.white};
+    gap: 0.9rem;
+  `,
+
+  WritingBtn: styled.button`
+    display: flex;
+    flex-direction: column;
+    width: 34.3rem;
+    padding: 2.3rem 1.8rem;
+    background-color: ${({ theme }) => theme.colors.gray400};
+    border-radius: 0.8rem;
+  `,
+
+  WritingTextWrapper: styled.div`
+    display: flex;
+    margin-top: 1rem;
+    gap: 9rem;
+  `,
+
+  ViewBtn: styled.button`
+    display: flex;
+    width: 34.3rem;
+    padding: 2.3rem 1.8rem;
+    align-items: flex-end;
+    gap: 13rem;
+    background-color: ${({ theme }) => theme.colors.gray400};
+    border-radius: 0.8rem;
   `,
 };
