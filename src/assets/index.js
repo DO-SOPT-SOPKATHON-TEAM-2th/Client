829f2bff581a538ad1b08efc68d9dc4d89a3fdc5
Doyun Lee <doyn511@ajou.ac.kr>
2023-11-26 05:28:10 +0900

    [feat / Main] icon import

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,6 @@
+import NavigationLogoSmallIc from './icons/navigationLogoSmallIc.svg?react';
 import NavigationLeftIc from './icons/post/NavigationLeftIc.svg?react';
 import BoardLikeOffIc from './icons/post/boardLikeOffIc.svg?react';
 import BoardLikeOnIc from './icons/post/boardLikeOnIc.svg?react';
 
-export { BoardLikeOffIc, BoardLikeOnIc, NavigationLeftIc };
-
+export { BoardLikeOffIc, BoardLikeOnIc, NavigationLeftIc, NavigationLogoSmallIc };
