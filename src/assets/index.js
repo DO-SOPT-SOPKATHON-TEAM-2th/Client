1ff9c4e3bf86881e3bbb29baf59475c112f588de
namdaeun <nde40345@gmail.com>
2023-11-26 03:46:10 +0900

    [docs / Post] 아이콘 import, export

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,4 +2,5 @@ import NavigationLeftIc from './icons/post/NavigationLeftIc.svg?react';
 import BoardLikeOffIc from './icons/post/boardLikeOffIc.svg?react';
 import BoardLikeOnIc from './icons/post/boardLikeOnIc.svg?react';
 
-export { NavigationLeftIc, BoardLikeOffIc, BoardLikeOnIc };
+export { BoardLikeOffIc, BoardLikeOnIc, NavigationLeftIc };
+
