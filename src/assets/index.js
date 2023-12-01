cb4d50284eebee05535eacf25eacd3296db35d0f
Jeon Eonseok <jseokeon@gmail.com>
2023-12-02 08:31:51 +0900

    fix: import error 수정

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,7 @@
 import MainWriteIc from './icons/mainWriteIc.svg?react';
 import NavigationLogoSmallIc from './icons/navigationLogoSmallIc.svg?react';
 import NavigationRightIc from './icons/navigationRightIc.svg?react';
-import NavigationLeftIc from './icons/post/NavigationLeftIc.svg?react';
+import NavigationLeftIc from './icons/post/navigationLeftIc.svg?react';
 import BoardLikeOffIc from './icons/post/boardLikeOffIc.svg?react';
 import BoardLikeOnIc from './icons/post/boardLikeOnIc.svg?react';
 export { BoardLikeOffIc, BoardLikeOnIc, MainWriteIc, NavigationLeftIc, NavigationLogoSmallIc, NavigationRightIc };
